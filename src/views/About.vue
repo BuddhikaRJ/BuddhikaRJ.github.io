41dc17cc6bf96bcdeebc46d219fb795fe4408ff5
BuddhikaRJ <buddhikarj@gmail.com>
2023-02-08 08:09:56 +0530

    project with github actions

---
+++ src/views/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="margin-bottom: 80px;">
+    <h1>Hello!</h1>
+
+    <div class="paragraph">
+      <div>
+        I'm <strong>Buddhika</strong>, a game developer, living in Sri Lanka.<br/>
+        I spend my days making games for a local game dev company.
+      </div>
+
+      <div style="margin-top: 20px;">I make <router-link to="/game-projects">Casual Mobile Games</router-link> and <router-link to="/music-games">Music Games</router-link> using Pixi.Js. 
+      Also you can see some <router-link to="/leisure-study">personal Unity games and projects</router-link> that I have created.</div>
+
+    </div>
+
+    <div class="photo">
+      <img src="img/avatar.png" alt="Avatar of Buddhika" /> 
+    </div>
+
+    <div style="clear:both"></div>
+
+  </div>
+</template>
+
+<style scoped>
+.paragraph {
+  max-width: 700px;
+  margin-bottom: 20px;
+}
+
+.photo {
+  margin-top: 50px;
+  text-align: center;
+}
+
+@media only screen and (min-width: 620px){
+  .paragraph {
+    float: left;
+  }
+
+  .photo {
+    float: left;
+    padding: 10px;
+    padding-left: 80px;
+  }
+}
+
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <h1>Resume</h1>
+
+    <div class="paragraph">
+      I'm <strong>Buddhika</strong>, a Game Developer currently working in a Sri Lankan Game Dev company.
+      <br />
+      <br />Right ow I'm working as a lead developer with a team of game devs to deliver new games to our imi games platform.
+      <br />
+      <br />
+
+      <div style="margin-top:20px; margin-bottom:20px;">
+        <a class="download-link" href="d/Buddhika Jayawickrama - resume.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Download resume as PDF</a>
+        
+        <div style="clear:both"></div>
+      </div>
+    </div>
+    <div class="photo">
+      <img src="img/resume-photo.png" alt="Photo of Buddhika" />
+    </div>
+
+    <div style="clear:both"></div>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+
+</script>
+
+<style scoped>
+h2 {
+  font-weight: 100;
+  font-size: 2em;
+  margin: 0px;
+  padding: 0px;
+  padding-top: 50px;
+}
+h3 {
+  font-weight: 100;
+  font-size: 1.6em;
+  margin: 0px;
+  padding: 0px;
+  padding-top: 30px;
+}
+h4 {
+  font-weight: 100;
+  font-size: 1.3em;
+  margin: 0px;
+  padding: 0px;
+  padding-top: 15px;
+  font-style: italic;
+}
+
+.paragraph {
+  max-width: 700px;
+}
+
+.download-link {
+  float:left;
+  margin-right: 50px;
+}
+
+.full-content div {
+  max-width: 800px;
+}
+
+.skill-set h3 {
+  padding-bottom: 10px;
+}
+
+.photo img {
+  height: 300px;
+  width: 300px;
+}
+
+.tech-stack {
+  font-style: italic;
+}
+
+.photo {
+  margin-top: 50px;
+  text-align: center;
+}
+
+@media only screen and (min-width: 620px){
+  .paragraph {
+    float: left;
+  }
+
+  .photo {
+    float: left;
+    padding: 30px;
+    padding-left: 80px;
+  }
+
+  .skill-set {
+    float: left;
+    padding-right: 60px; 
+  }
+}
+</style>
